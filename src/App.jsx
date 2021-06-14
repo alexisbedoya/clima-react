42161fcf39a90c984e358897b0b20a3a76d7aa30
alexisbedoya <39450793+alexisbedoya@users.noreply.github.com>
2021-06-14 01:35:04 +0000

    Add material icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-
+import {Button, AppBar, IconButton, Toolbar, Typography} from "@material-ui/core";
+import { Menu as MenuIcon } from '@material-ui/icons'
 
 
 function App() {
   return (
-    <div> hello work</div>
+    <div> 
+      <Button variant="text" color="primary">
+        color
+      </Button>
+      <AppBar position="fixed" color="primary">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 
